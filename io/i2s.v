6ba5573e2519670c925cb12a52d4aa4306a463f2
Annie Lee <noah.lee@duke.edu>
2024-04-11 11:51:44 -0400

    erics cool fix for edge detection

--- io/i2s.v
+++ io/i2s.v
@@ -6,7 +6,7 @@ module i2s(
 
     reg [15:0] audio_reg_left, audio_reg_right;
     reg old_word_clock;
-    reg [16:0] data_reg;
+    reg [15:0] data_reg;
  
     always @(posedge sys_clock)begin //yes, you could technically get by with one register.
         audio_reg_left = audio_data;
@@ -20,21 +20,28 @@ module i2s(
     sys_counter_wide #(31) wordclock(~bit_clock, reset, word_clock); //weird but its on the not, i know right
 
     initial begin
-        data_reg = 17'b11111111111111111;
+        data_reg = 16'hffff;
     end
 
+    reg word_rising, word_falling;
     always @(posedge bit_clock) begin
+         if(~old_word_clock & word_clock)begin //edge detection ! L to R
+            word_rising <= 1'b1;
+        end if(old_word_clock & ~word_clock)begin  //edge detection ! R to L
+            word_falling <= 1'b1;
+        end
         old_word_clock <= word_clock;
+
     end
 
     always @(negedge bit_clock) begin
         data_reg <= data_reg << 1;
-        if(~old_word_clock & word_clock)begin //edge detection ! L to R
-            data_reg <= {1'b0, audio_reg_right};
-        end if(old_word_clock & ~word_clock)begin  //edge detection ! R to L
-            data_reg <= {1'b0, audio_reg_left};
+        if(word_rising == 1)begin //edge detection ! L to R
+            data_reg <= audio_reg_right;
+        end if(word_falling == 1)begin  //edge detection ! R to L
+            data_reg <= audio_reg_left;
         end
     end
 
-    assign data_bit = data_reg[16];
+    assign data_bit = data_reg[15];
 endmodule
