ec9b90fa093d15b2a638ac3396d977c715a881c9
Eric Liu <el260@duke.edu>
2024-04-29 01:18:19 -0400

    clean dct of debug statements

--- Test Files/Assembly Files/dct.s
+++ Test Files/Assembly Files/dct.s
@@ -1,26 +1,6 @@
 nop                 # discrete cosine transform algorithm
 addi    $sp,    $0,     4096    # initialize sp to end of memory
 
-# addi    $t1,    $0,     31
-# addi    $t0,    $0,     0
-# _test_loop:
-#     blt     $t1,    $t0,    _end_test_loop
-#     sw      $t0,    7($t0)
-#     addi    $t0,    $t0,    1
-#     j       _test_loop
-
-# _end_test_loop:
-#     addi    $a0,    $0,     7
-#     addi    $a1,    $0,      32
-#     addi    $a2,    $0,     40
-#     jal     dct_plus_minus
-
-#     addi    $a0,    $0,     7
-#     addi    $a1,    $0,     80
-#     jal     print_array
-
-# j halt
-
 # ====MAIN====
 _poll_audio_buffer:
     lw      $s0,    8195($0)
@@ -30,7 +10,6 @@ _poll_audio_buffer:
 _audio_buffer_ready:
 addi    $s0,    $0,    -65536  # 17 bit minimum?
 addi    $t5,    $0,     -65536  # negative upper half
-# disp    $s0
 addi    $s2,    $0,     511      # loop bounds     
 addi    $t1,    $0,     32768   # sign bit mask
 addi    $s1,    $0,     0   
@@ -40,18 +19,11 @@ _load_audio_buffer_loop:
     sll     $s4,    $s4,    16
     sra     $s4,    $s4,    16
 
-    # disp    $s4
     sw      $s4,    0($s1)
     addi    $s0,    $s0,    1
     addi    $s1,    $s1,    1
 j       _load_audio_buffer_loop
 _done_load_buffer:
-# addi    $s7,    $0,    -1
-# disp    $s7
-# disp    $s7
-# disp    $s7
-# disp    $s7
-# add     $s7,    $0,     $0
 
 addi    $s0,    $0,     0
 addi    $s1,    $0,     512
@@ -62,23 +34,24 @@ addi    $a2,    $s2,    0
 addi    $a3,    $0,     0
 jal     dct
 
-    sll     $t1,    $s1,    1
-    addi    $t1,    $t1,    -1      # loop bounds
-    add     $t1,    $t1,    $s2
-    addi    $t0,    $s2,    0
+sll     $t1,    $s1,    1
+addi    $t1,    $t1,    -1      # loop bounds
+add     $t1,    $t1,    $s2
+addi    $t0,    $s2,    0
 
+lw      $t2,    0($t0)
+sra     $t2,   $t2,    9        # NOTE: change this when changing DCT size!
+sw      $t2,    0($t0)
+addi    $t0,    $t0,    1
+
+_dct_normalize_loop:
+    blt     $t1,    $t0,    _dct_normalize_loop_done
     lw      $t2,    0($t0)
-    sra     $t2,   $t2,    9        # NOTE: change this when changing DCT size!
+    sra     $t2,   $t2,    8        # NOTE: change this when changing DCT size!
     sw      $t2,    0($t0)
     addi    $t0,    $t0,    1
-    _dct_normalize_loop:
-        blt     $t1,    $t0,    _dct_normalize_loop_done
-        lw      $t2,    0($t0)
-        sra     $t2,   $t2,    8        # NOTE: change this when changing DCT size!
-        sw      $t2,    0($t0)
-        addi    $t0,    $t0,    1
-        j       _dct_normalize_loop
-    _dct_normalize_loop_done:
+    j       _dct_normalize_loop
+_dct_normalize_loop_done:
 
 addi    $s4,    $0,     0       # iteration lower bound
 addi    $s3,    $0,     15      # iter variable
@@ -103,12 +76,6 @@ j   _poll_audio_buffer
 # a2: new addr
 # a3: is inplace
 dct:
-    # disp    $a0
-    # disp    $a1
-    # disp    $a2
-    # disp    $a3
-    # addi    $at,    $at,    -1
-    # disp    $at
     nop  # FIX: bypassing error here
     addi    $t9,    $0,     3
     blt     $a1,    $t9,    _dct_base_case  # if N <= 2, go to base case. Second exit point. Ugh.
@@ -123,7 +90,6 @@ dct:
     add     $s0,    $a0,    0       # s0 = &vector[0]
     sra     $s1,    $a1,    1       # s1 = half = n / 2
     addi    $s2,    $a2,    0     # s2 = &vector[len(vector)/2], i.e. start of second half
-    # addi    $a2,    $s0,    0
 
     bne     $a3,    0,  _dct_plus_minus_inplace
         nop                     # BUG: bypassing messes up here, branch/jal race condition?
@@ -134,24 +100,14 @@ dct:
         nop
         addi    $a3,    $0,     0
         jal dct_plus_minus
-        # addi    $s0,    $s2,    0
-        # disp    $s0
     _dct_begin_mul:
-
-    # addi    $a0,    $s2,    0
-    # addi    $a1,    $0,     8
-    # jal print_array
-    # addi    $t9,    $0,    -1
-    # disp    $t9
     # IDEA: use length of array as index of LUTs! LUT for len 4 goes from mem[4:7], lut for len 8 goes from mem[8:15], etc!
-
     # vector_mul(&vector[half], half, &dct_lut[half])
     add     $a0,    $s0,    $s1       # a0 = &vector[half]
     addi    $a1,    $s1,    0       # a1 = half
     addi    $a2,    $s1,    16384   # dct_lut[len/2]
     jal		vector_mul
 
-    # jal print_array
     addi    $a2,    $a0,    0
     jal     dct         # dct(vector[half:])
 
@@ -164,10 +120,6 @@ dct:
     addi    $t0,    $t0,    1       # t0 = &vector[half + 1]
     add     $t1,    $t0,    $s1     # t1 = end of vector
     addi	$t1,    $t1,    -2      # t1 = vector[len-1]
-
-    # disp    $s1
-    # disp    $t0
-    # disp    $t1
 
     _dct_beta_sum_loop:
         blt     $t1,    $t0,    _dct_zip
@@ -182,13 +134,6 @@ dct:
     addi    $a0,    $s0,    0
     addi    $a1,    $s1,    0
     jal     zip_array
-
-    # addi    $a0,    $s0,    0
-    # sll     $a1,    $s1,    1
-    # jal     print_array 
-    # addi    $at,    $0,    -1
-    # disp    $at
-    # disp    $at
 
     lw      $s0,    0($sp)
     lw      $s1,    1($sp)
@@ -210,12 +155,6 @@ jr      $ra
         sw      $t2,    0($a0)
         sw      $t3,    1($a0)
         addi    $a0,    $s2,    0
-
-        # disp    $t2
-        # disp    $t3
-        # addi    $t9,    $0,    -1
-        # disp    $t9
-
         jr      $ra
 
 
@@ -315,8 +254,6 @@ vector_mul:
 
         xpmul   $t5,    $t3,    $t4
         xpmul   $t8,    $t6,    $t7
-        # sra     $t5,    $t5,    16
-        # sra     $t8,    $t8,    16
         sw      $t5,    0($t0)
         sw      $t8,    1($t0)
 
@@ -351,11 +288,6 @@ zip_array:
         j       _zip_array_copy
 
     _zip_array_copy_done:
-    # addi    $a0,    $sp,    0
-    # addi    $a1,    $t1,    0
-    # jal     print_array
-
-    # j halt
     addi    $t3,    $t0,    0       # loop min bounds
     add     $t2,    $t0,    $t1
     addi    $t2,    $t2,    -1      # t2 = half - 1
@@ -369,13 +301,6 @@ zip_array:
         addi    $t5,    $t5,    -2
         j   _zip_array_a_expand_loop
     _zip_array_a_expand_loop_done:
-    # addi    $a0,    $t0,    0
-    # sll     $a1,    $t1,    1
-    # jal     print_array
-
-    # j halt
-
-
     addi    $t2,    $sp,    0       
     add     $t3,    $sp,    $t1     # loop min bound
     addi    $t3,    $t3,    -1      # t2 = vec_b[len -1]
@@ -388,13 +313,10 @@ zip_array:
         addi    $t2,    $t2,    1
         addi    $t5,    $t5,    2
         j   _zip_array_b_expand_loop
-    # _zip_array_b_expand_loop_done:
 
     _zip_array_done:
         add    $sp,    $sp,    $t1 
         jr  $ra
-    # # disp    $a0
-    # # disp    $a1
 
 
 # ====print array====
